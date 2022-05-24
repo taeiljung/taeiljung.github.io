7b5549ade80086108caadf1ce81e5377612efb18
taeil_jung <brilliant_427@naver.com>
2022-05-24 16:08:09 +0900

    1
    11

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ window.onload = main();
 function main(){
 }
 
-const circle = document.querySelector(".circle");
+/*const circle = document.querySelector(".circle");
 
 document.addEventListener("mousemove", (e) => {
 		console.log(e);
@@ -11,4 +11,24 @@ document.addEventListener("mousemove", (e) => {
         const mouseY = e.clientY;
         circle.style.left = mouseX + 'px';
         circle.style.top = mouseY + 'px';
-});
+});*/
+
+const content = "_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._";
+const text = document.querySelector(".typing");
+console.log(text.textContent)
+let index = 0;
+
+ function sleep(delay){ 
+     const start = new Date().getTime(); 
+     while (new Date().getTime() < start + delay); 
+    }
+
+/*function typing(){
+    text.textContent += content[index++];
+    if(index > content.length){
+        text.textContent = ""
+        index = 0;
+        sleep(3000);
+    }
+}
+setInterval(typing, 200)*/

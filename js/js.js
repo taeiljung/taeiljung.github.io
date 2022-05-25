b396d6b60ad047849fe6a7d3225bb37deb707fcf
taeil_jung <brilliant_427@naver.com>
2022-05-25 11:14:54 +0900

    add particles
    add particles

--- js/js.js
+++ js/js.js
@@ -1,18 +1,14 @@
 window.onload = main();
-
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particle'), {
+    dotColor: '#fff',
+    lineColor: '#fff'
+  });
+}, false);
 function main(){
 }
 
-/*const circle = document.querySelector(".circle");
-
-document.addEventListener("mousemove", (e) => {
-		console.log(e);
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        circle.style.left = mouseX + 'px';
-        circle.style.top = mouseY + 'px';
-});*/
-
+/*
 const content = "_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._";
 const text = document.querySelector(".typing");
 console.log(text.textContent)
@@ -21,7 +17,7 @@ let index = 0;
  function sleep(delay){ 
      const start = new Date().getTime(); 
      while (new Date().getTime() < start + delay); 
-    }
+    }*/
 
 /*function typing(){
     text.textContent += content[index++];

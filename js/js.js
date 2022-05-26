6c4dafc82d46683f2780f6873dfd1de80d26761a
taeil_jung <brilliant_427@naver.com>
2022-05-26 14:05:24 +0900

    1
    1

--- js/js.js
+++ js/js.js
@@ -3,12 +3,26 @@ function main(){
 
 }
 
+/*button 3개를 만들어 색상변경 기능구현*/
+
+/*slide part*/
+var menu = document.querySelector("#app > div > div.grid.g1.selected > div.title.a4 > a");
+var xbutton = document.querySelector("body > div.menu > .quit");
+var slider = document.querySelector("body > div.menu");
+menu.addEventListener('click',function(){
+    slider.classList.add('click');
+})
+xbutton.addEventListener('click',function(){
+    slider.classList.remove('click');
+})
+
+
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particle'), {
-    dotColor: '#999',
-    lineColor: '#999',
-    particleRadius:'4'
-    /*parallaxMultiplier:'100'*/
+    dotColor: '#333',
+    lineColor: '#333',
+    particleRadius:'5',
+    parallaxMultiplier:'20'
   });
 }, false);
 
